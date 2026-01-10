2c087868903320dd00b816208483ad6e42cc6521
Akuma2 <akuma2devog@gmail.com>
2026-01-10 11:44:31 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,13 @@
+import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 from pymongo import MongoClient
 
-BOT_TOKEN = "7543377011:AAFPhEM3rhC-T0yPpvJ9xXejatjCv39zT9o"
-MONGO_URI = "mongodb+srv://akukamibot:%3A%40kum%40d3v0g@cluster0.bgfgveu.mongodb.net/?appName=Cluster0"
+BOT_TOKEN = os.getenv("7543377011:AAFPhEM3rhC-T0yPpvJ9xXejatjCv39zT9o")
+MONGO_URI = os.getenv("mongodb+srv://akukamibot:%3A%40kum%40d3v0g@cluster0.bgfgveu.mongodb.net/?appName=Cluster0")
+
+client = MongoClient(MONGO_URI)
+db = client.animebot
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
