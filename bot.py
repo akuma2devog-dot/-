442b156c7a9bbe08c9daadca181575002a70204d
Akuma2 <akuma2devog@gmail.com>
2026-01-10 11:31:57 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,19 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from pymongo import MongoClient
+
+BOT_TOKEN = "7543377011:AAFPhEM3rhC-T0yPpvJ9xXejatjCv39zT9o"
+MONGO_URI = "mongodb+srv://akukamibot:%3A%40kum%40d3v0g@cluster0.bgfgveu.mongodb.net/?appName=Cluster0"
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "✅ Bot is alive!\nMongoDB connected.\nMore features coming soon."
+    )
+
+def main():
+    app = ApplicationBuilder().token(BOT_TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
