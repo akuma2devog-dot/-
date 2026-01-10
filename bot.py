dc8858ca18486de6f7a8f0eb8cc2f3d37c12db9b
Akuma2 <akuma2devog@gmail.com>
2026-01-10 15:26:15 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,29 @@
 import os
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import (
+    ApplicationBuilder,
+    CommandHandler,
+    ContextTypes,
+    CallbackQueryHandler
+)
+from pymongo import MongoClient
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+MONGO_URI = os.getenv("MONGO_URI")
 
-if not BOT_TOKEN:
-    raise RuntimeError("BOT_TOKEN is NOT loaded from environment variables")
+if not BOT_TOKEN or not MONGO_URI:
+    raise RuntimeError("Missing environment variables")
+
+# MongoDB
+client = MongoClient(MONGO_URI)
+db = client.animebot
+anime_col = db.anime
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("✅ Bot is alive!")
+    await update.message.reply_text(
+        "🎬 Welcome to AKUKAMI Anime Bot\n\n"
+        "Use channel buttons to get episodes."
+    )
 
 def main():
     app = ApplicationBuilder().token(BOT_TOKEN).build()
