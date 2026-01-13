375e24634e446705c7cc492712f2435f3ad53b6b
Akuma2 <akuma2devog@gmail.com>
2026-01-13 12:17:17 +0530

    Create server.py

---
+++ server.py
@@ -0,0 +1,11 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from config import PORT
+
+class HealthHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"OK")
+
+def run_server():
+    HTTPServer(("0.0.0.0", PORT), HealthHandler).serve_forever()
