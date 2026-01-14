c039b4b7f3ecbcecacf36d2d25f1d281f31ac0c3
Akuma2 <akuma2devog@gmail.com>
2026-01-14 07:50:07 +0530

    Delete helpers directory

--- helpers/episode.py
+++
@@ -1,8 +0,0 @@
-from db.mongo import episodes
-
-def get_next_episode(anime, season, quality):
-    last = episodes.find_one(
-        {"anime": anime, "season": season, "quality": quality},
-        sort=[("episode", -1)]
-    )
-    return last["episode"] + 1 if last else 1
