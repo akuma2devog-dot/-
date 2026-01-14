15913b53b39374e05328cb5821c0e5b831e3b4a3
Akuma2 <akuma2devog@gmail.com>
2026-01-14 07:49:47 +0530

    Delete db directory

--- db/mongo.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-from config import MONGO_URI
-
-client = MongoClient(MONGO_URI)
-db = client.animebot
-
-episodes = db.episodes
-config_col = db.config
-templates = db.templates
